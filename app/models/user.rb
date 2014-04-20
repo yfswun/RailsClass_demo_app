b731f86f7268e752495d9e9aa911a5b9e0b9f821
Sylvia Wun <yfswun@yahoo.com>
2014-04-19 20:07:21 -0700

    Clean up users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ActiveRecord::Base
 
-  # before_save { self.email = email.downcase }
   before_save { email.downcase! }
 
   has_many :microposts
 
-  validates_presence_of :name
-  validates_length_of :name, maximum: 50
+  validates :name, presence: true, length: { maximum: 50 }
 
-  validates_presence_of :email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   validates_length_of :password, minimum: 6
